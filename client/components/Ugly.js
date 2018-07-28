dc358a9b2069b6c2622584032d8f7307baa9a45a
Andy Castro <descodms@gmail.com>
2018-07-28 04:53:17 -0300

    updating

---
+++ client/components/Ugly.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Ugly = React.createClass({
+  render() {
+    return (
+      <div className="ugly-photo">
+        <h1>I'm the ugly</h1>{' '}
+      </div>
+    );
+  },
+});
+
+export default Ugly;
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { render } from 'react-dom';
+import css from './styles/style.styl';
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+import { IndexRoute, Router, Route, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main} />
+  </Router>
+);
+
+render(
+  <Main>
+    <p>ae</p>
+  </Main>,
+  document.getElementById('root'),
+);
